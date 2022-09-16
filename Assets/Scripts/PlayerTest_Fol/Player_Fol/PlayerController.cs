043f9a947753496bed747847d261406a59760f3f
Yaseen <shaikhyaseen6496@gmail.com>
2022-09-16 19:22:48 +0530

    on the right track. maybe. player controller

--- Assets/Scripts/PlayerTest_Fol/Player_Fol/PlayerController.cs
+++ Assets/Scripts/PlayerTest_Fol/Player_Fol/PlayerController.cs
@@ -17,8 +17,9 @@ public class PlayerController : MonoBehaviour
     PlayerState currentPlayerState;
     PlayerState[] playerStates;
     //----------------------------------------------------//
-    Vector3 colliderHalfExtents = new Vector3(1f / 2, 2f / 2, 1f / 2);
     Vector3 gravityAcc = new Vector3(0, -0.02f, 0), gravityVelo;
+    Vector3 playerDir;
+    float hori, vert;
     //--------------------------------------------------//
 
     void Start()
@@ -44,33 +45,42 @@ public class PlayerController : MonoBehaviour
 
     // void OnDrawGizmos()
     // {
-    //     float speed = 0.05f;
-    //     float hori = Input.GetAxisRaw("Horizontal");
-    //     float velo = Input.GetAxisRaw("Vertical");
+    //     float radius = playerCollider.radius;
+    //     float speed = 0.03f;
+
+
+    //     playerDir = player.transform.forward;
     //     Vector3 oldPos = player.transform.position;
-    //     Vector3 newPos = new Vector3(oldPos.x + (hori * speed), oldPos.y, oldPos.z + (velo * speed));
-    //     Vector3 origin = new Vector3(newPos.x, newPos.y + 2, newPos.z);
+    //     Vector3 newPos = oldPos + (playerDir * speed);
 
-    //     RaycastHit raycastHitInfo;
-    //     if (Physics.Raycast(origin, Vector3.down, out raycastHitInfo, 5f, groundLayer))
+    //     RaycastHit raycastHit;
+    //     if (Physics.Raycast(oldPos, Vector3.down, out raycastHit, 0.5f, groundLayer))
     //     {
-    //         Gizmos.color = Color.white;
-    //         Gizmos.DrawLine(origin, raycastHitInfo.point);
-    //         Vector3 normal = raycastHitInfo.normal;
-    //         Vector3 reflected = Vector3.Reflect(Vector3.down, normal);
+    //         Vector3 surfacePoint = raycastHit.point;
+    //         Vector3 surfaceNormal = raycastHit.normal;
 
-    //         RaycastHit raycastHitInfo2;
-    //         Physics.Raycast(raycastHitInfo.point, reflected, out raycastHitInfo2, 5, groundLayer);
-    //         Vector3 otherPoint = raycastHitInfo.point + reflected;
+    //         DrawVector(surfacePoint, surfaceNormal, Color.black);
+    //         DrawVector(surfacePoint, playerDir, Color.magenta);
 
-    //         Gizmos.color = Color.black;
-    //         Gizmos.DrawLine(raycastHitInfo.point, otherPoint);
+    //         Vector3 c1 = Vector3.Cross(surfaceNormal, playerDir);
+    //         DrawVector(surfacePoint, c1, Color.red);
+
+    //         Vector3 c2 = Vector3.Cross(c1, surfaceNormal);
+    //         DrawVector(surfacePoint, c2, Color.gray);
+    //     }
+
+    //     void DrawVector(Vector3 position, Vector3 dir, Color color)
+    //     {
+    //         Gizmos.color = color;
+    //         Gizmos.DrawLine(position, position + dir);
     //     }
     // }
 
-
     void Update()
     {
+        hori = Input.GetAxisRaw("Horizontal");
+        vert = Input.GetAxisRaw("Vertical");
+
         currentPlayerStateE = currentPlayerState.Update();
         currentPlayerState = playerStates[(int)currentPlayerStateE];
         currentPlayerState.Action();
@@ -78,141 +88,87 @@ public class PlayerController : MonoBehaviour
         if (GroundCheck())
         {
             // Debug.Log("Grounded");
-            MoveA(player.transform.forward);
+            MoveA();
             gravityVelo = Vector3.zero;
         }
         else
         {
             // Debug.Log("Gravity");
-            ApplyGravity();
+            // ApplyGravity();
         }
-
-        MoveA(player.transform.forward);
 
     }
     bool GroundCheck()
     {
         return CheckCapsule(playerCollider, player.transform.position - (Vector3.up * 0.01f));
     }
-    /*
-    void Move(vector3 dir) {
-        dir = dir.normalize;
-        if(notGrounded)
-            falling;
-        
-        check if move possible
-            cast forward.
-            if hit
-                then check if we can climb over
-                cast up from new pos
-                if hit 
-                    cant move forward
-                    move side ways using normals
-                else 
-                    move forword to newPos
-            else
-                move forword regularly   
-    }
-    */
+
     GameObject marker, marker2, marker3, marker4, marker5, marker6, marker7;
-    void MoveA(Vector3 dir)
+    void MoveA()
     {
         float radius = playerCollider.radius;
         float speed = 0.03f;
-        float hori = Input.GetAxisRaw("Horizontal");
-        float Vert = Input.GetAxisRaw("Vertical");
+        float stepHeight = 1f;
 
-        if (hori == 0 && Vert == 0)
+        if (hori == 0 && vert == 0)
             return;
 
-        dir = new Vector3(hori, 0, Vert).normalized;
+        playerDir = new Vector3(hori, 0, vert).normalized;
+
+        Vector3 oldPos = player.transform.position;
+        Vector3 newPos = oldPos + (playerDir * speed);
 
 
-        float stepHeight = 1.1f;
-        GameObject.Destroy(marker);
-        GameObject.Destroy(marker2);
-        GameObject.Destroy(marker3);
-        GameObject.Destroy(marker6);
-        GameObject.Destroy(marker7);
-        Vector3 oldPos = player.transform.position; // + (Vector3.up * 0.01f); // offset
-        Vector3 newPos = oldPos + (dir * speed);
-
-        RaycastHit raycastHitWallCheck;
-        if (CheckCapsuleCast(playerCollider, oldPos, out raycastHitWallCheck, dir, speed))
+        RaycastHit raycastHit;
+        if (Physics.Raycast(oldPos + (Vector3.up * 0.25f), Vector3.down, out raycastHit, 0.5f, groundLayer))
         {
-            Debug.Log("HitWall!");
-            // marker = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            // marker.transform.position = raycastHitWallCheck.point;
-            // marker.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-            // marker.GetComponent<Renderer>().material.SetColor("_Color", Color.black);
+            
+            Debug.Log("In");
 
+            Vector3 surfacePoint = raycastHit.point;
+            Vector3 surfaceNormal = raycastHit.normal;
+            Vector3 c1 = Vector3.Cross(surfaceNormal, playerDir);
+            Vector3 c2 = Vector3.Cross(c1, surfaceNormal);
 
-            //ray up to surface of wall
-            RaycastHit raycastHitUpToWallSurface;
-            Vector3 wallSurfaceorigin = newPos + (Vector3.up * stepHeight);
+            float angle = Vector3.Angle(playerDir, surfaceNormal);
 
-            marker3 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            marker3.transform.position = wallSurfaceorigin;
-            marker3.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-            marker3.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
-            marker7 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            marker7.transform.position = wallSurfaceorigin + (Vector3.down * stepHeight);
-            marker7.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-            marker7.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
-
-
-            if (CheckCapsuleCast(playerCollider, wallSurfaceorigin, out raycastHitUpToWallSurface, Vector3.down, stepHeight))
+            if (angle < 45)
             {
-                Debug.Log("Hit Surface");
-                marker2 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                marker2.transform.position = raycastHitUpToWallSurface.point;
-                marker2.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
 
-                if (raycastHitUpToWallSurface.point.y - oldPos.y < stepHeight)
+                Vector3 originForward = newPos + (Vector3.up * stepHeight) + (playerDir * radius);
+                Vector3 originBackward = newPos + (Vector3.up * stepHeight) - (playerDir * radius);
+
+                // climb over wall
+                RaycastHit raycastHit1;
+                if (Physics.Raycast(originForward, Vector3.down, out raycastHit1, stepHeight, groundLayer))
                 {
-                    Debug.Log("Value less than");
-                    // check if space above
-                    if (!CheckCapsule(playerCollider, raycastHitUpToWallSurface.point + (Vector3.up * 0.05f)))
+                    Vector3 wallTopSurfacePoint = raycastHit1.point;
+                    if (!CheckCapsule(playerCollider, wallTopSurfacePoint - (playerDir * radius)))
                     {
-                        Debug.Log("space above");
-                        marker6 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                        marker6.transform.position = raycastHitUpToWallSurface.point;
-                        marker6.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-                        marker6.GetComponent<Renderer>().material.SetColor("_Color", Color.cyan);
-
-
-
-                        player.transform.position = raycastHitUpToWallSurface.point+ (Vector3.up * 0.05f);
+                        player.transform.position = wallTopSurfacePoint - (playerDir * radius);
                     }
                 }
             }
+            else
+            {
+                Vector3 origin = newPos+ (Vector3.up * stepHeight);
+                RaycastHit raycastHit1;
+                if (Physics.Raycast(origin, Vector3.down, out raycastHit1, stepHeight, groundLayer))
+                {
+                    player.transform.position = raycastHit1.point;
+                }
+            }
+        }
 
-        }
-        else
-        {
-            player.transform.position = newPos;
-        }
     }
 
     bool CheckCapsuleCast(CapsuleCollider collider, Vector3 position, out RaycastHit raycastHit, Vector3 dir, float maxDist)
     {
-        Destroy(marker4);
-        Destroy(marker5);
         Vector3 posOffset = position - collider.transform.position;
         float quaterHeight = collider.height * 0.25f;
         float radius = collider.radius;
         Vector3 point1 = collider.transform.TransformPoint(new Vector3(0, quaterHeight, 0)) + posOffset;
         Vector3 point2 = collider.transform.TransformPoint(new Vector3(0, collider.height - quaterHeight, 0)) + posOffset;
-
-        marker4 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        marker4.transform.position = point1;
-        marker4.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-        marker4.GetComponent<Renderer>().material.SetColor("_Color", Color.cyan);
-        marker5 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        marker5.transform.position = point2;
-        marker5.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-        marker5.GetComponent<Renderer>().material.SetColor("_Color", Color.cyan);
-
         return Physics.CapsuleCast(point1, point2, radius, dir, out raycastHit, maxDist, groundLayer);
 
     }
@@ -222,8 +178,6 @@ public class PlayerController : MonoBehaviour
         float quaterHeight = collider.height * 0.25f;
         Vector3 point1 = collider.transform.TransformPoint(new Vector3(0, quaterHeight, 0)) + posOffset;
         Vector3 point2 = collider.transform.TransformPoint(new Vector3(0, collider.height - quaterHeight, 0)) + posOffset;
-
-
         return Physics.CheckCapsule(point1, point2, collider.radius, groundLayer);
     }
 
